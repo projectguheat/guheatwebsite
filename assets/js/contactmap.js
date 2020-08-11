78afb4362af5d2da82986fb7816d52609442dc7a
Ace Santos <jacessantos@gmail.com>
2020-08-11 21:04:33 +0800

    august 11 9pm

---
+++ assets/js/contactmap.js
@@ -0,0 +1,29 @@
+//CONTACT MAP
+var mymap = L.map('contactmap').setView([14.656494, 121.070576], 18);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+	L.marker([14.656411, 121.070830]).addTo(mymap)
+		.bindPopup("Project GUHeat Office").bindPopup();
+
+	L.marker([14.656460, 121.070026]).addTo(mymap)
+		.bindPopup("ENVISaGe/DGE/TCAGP").bindPopup();
+
+
+
+	var popup = L.popup();
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(mymap);
+	}
